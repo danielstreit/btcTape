f8053cc03189dc2b536ab557a70fc87f917b53d6
Daniel Streit <Daniel.Streit@gmail.com>
2014-08-07 13:35:48 -0700

    Added bitfinex listener

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var logger = require('morgan');
 var bitstamp = require('./listeners/bitstamp');
+var bitfinex = require('./listeners/bitfinex');
 
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
 
 bitstamp.on('trade', handleTrade);
+bitfinex.on('trade', handleTrade);
 function handleTrade(trade) {
   io.sockets.emit('trade', trade);
 }
