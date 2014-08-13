d75dd46a8ec75a1310194a6aff7ebbbfbd14b189
Daniel Streit <Daniel.Streit@gmail.com>
2014-08-12 21:28:36 -0700

    Display trade data in a table

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,13 @@
 var socket = io.connect();
+var template = _.template('<tr><td><span data-livestamp="<%=date%>">a few seconds ago</span></td><td><%=price%></td><td><%=amount%></td><td><%=exchange%></td></tr>');
+var $tradeTable = $('table.table');
+var $headerRow = $('tr.headerRow:first');
+
 socket.on('connect', function() {
   console.log('hello');
 });
 socket.on('trade', function(trade) {
   console.log(trade);
+  trade.date = Math.floor(trade.date/1000);
+  $headerRow.after(template(trade));
 });
