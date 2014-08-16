c58009f2c01a9916c63f7cff2028880c9aea3ca9
Daniel Streit <Daniel.Streit@gmail.com>
2014-08-16 11:02:06 -0700

    Improved formatting of trades

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,27 @@
 var socket = io.connect();
-var template = _.template('<tr><td><span data-livestamp="<%=date%>">a few seconds ago</span></td><td><%=price%></td><td><%=amount%></td><td><%=exchange%></td></tr>');
+var template = _.template('<tr class="trade"><td><%=hours%>:<%=minutes%>:<%=seconds%></td><td><%=price%></td><td><%=amount%></td><td><%=exchange%></td></tr>');
 var $tradeTable = $('table.table');
 var $headerRow = $('tr.headerRow:first');
+var digits = 2; // Number of digits for formatting price and ammount
+
+var formatTrade = function(trade) {
+  var fTrade = {};
+  var date = new Date(trade.date);
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+  fTrade.hours = '' + date.getHours();
+  fTrade.minutes = minutes < 10 ? '0' + minutes : '' + minutes;
+  fTrade.seconds = seconds < 10 ? '0' + seconds : '' + seconds;
+  fTrade.price = trade.price.toFixed(digits);
+  fTrade.amount = trade.amount.toFixed(digits);
+  fTrade.exchange = trade.exchange;
+  return fTrade;
+}
 
 socket.on('connect', function() {
   console.log('hello');
 });
 socket.on('trade', function(trade) {
-  console.log(trade);
-  trade.date = Math.floor(trade.date/1000);
+  trade = formatTrade(trade);
   $headerRow.after(template(trade));
 });
